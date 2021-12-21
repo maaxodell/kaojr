51619cd7e0055751ecae9160a2b883c361070f31
PC <maxodell11@gmail.com>
2021-12-21 20:51:25 +1000

    cleanup

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 # Imports
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 import os
 
-# Environment variables
+# Load token
 load_dotenv()
 token = os.getenv("TOKEN")
 
+# Initialise bot client
 client = discord.Client()
 
 client.run(token)
