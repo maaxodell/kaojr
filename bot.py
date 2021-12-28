b30c14a3bb76c17488a784c8a9d60ca2fb674445
PC <maxodell11@gmail.com>
2021-12-28 21:07:15 +1000

    edit varaible name

--- bot.py
+++ bot.py
@@ -6,4 +6,4 @@ client = commands.Bot(command_prefix="!")
 async def on_ready():
     print("Connected")
 
-client.load_extension('commandCogs.HelpCommands')
+client.load_extension('commandCogs.helpCommands')
