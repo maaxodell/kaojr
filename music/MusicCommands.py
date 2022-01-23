b4cfde3cbe283e774da64701b1b1f41ab7fbadad
PC <maxodell11@gmail.com>
2022-01-24 00:34:43 +1000

    queue function working

--- music/MusicCommands.py
+++ music/MusicCommands.py
@@ -14,13 +14,22 @@ class MusicCommands(commands.Cog):
 
     queue = {}
 
+    class SongInfo:
+        def __init__(self, title, duration, requestedBy, source):
+            self.title = title
+            self.duration = duration
+            self.requestedBy = requestedBy
+            self.source = source
+
+    # Check for songs to play next
     def checkQueue(self, ctx, id):
         if self.queue[id] !={}:
             voice = ctx.guild.voice_client
-            source = self.queue[id].pop(0)
-            voice.play(source, after=lambda x=0: self.checkQueue(ctx, ctx.message.guild.id))
+            song = self.queue[id].pop(0)
+            voice.play(song.source, after=lambda x=0: self.checkQueue(ctx, ctx.message.guild.id))
+            ctx.send(":loud_sound: `{} [{}]` - {}".format(song.title, song.duration, song.requestedBy))
 
-    @commands.command(help="")
+    @commands.command(help="play some tunes in your voice channel")
     async def play(self, ctx, *, songName=None):
         with ctx.channel.typing():
             if not await self.checkMessage(ctx):
@@ -41,34 +50,37 @@ class MusicCommands(commands.Cog):
 
             # Find song on youtube
             yt = YoutubeSearch(songName, max_results = 1).to_json()
-            ytid = str(json.loads(yt)['videos'][0]['id'])
-            url = "https://www.youtube.com/watch?v={}".format(ytid)
+            ytdata = json.loads(yt)['videos'][0]
+            songID = ytdata['id']
+            songTitle = ytdata['title']
+            songDuration = ytdata['duration']
+            url = "https://www.youtube.com/watch?v={}".format(songID)
 
             # Set options for external libraries
             FFMOPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
             YDL_OPTIONS = {'format': 'bestaudio'}
             vc = ctx.voice_client
 
-            # Download and play audio
+            # Establish source of audio
             with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
                 info = ydl.extract_info(url, download=False)
                 url2 = info['formats'][0]['url']
-                title = info.get('title', None)
-                duration = str(timedelta(seconds=info.get('duration', None))) 
                 source = await discord.FFmpegOpusAudio.from_probe(url2,
                 **FFMOPTIONS)
                 
+            songInfo = self.SongInfo(songTitle, songDuration, ctx.author.name, source)
+
+            # Play song or add to queue if music already playing
             if vc.is_playing():
                 guild_id = ctx.message.guild.id
                 if guild_id in self.queue:
-                    self.queue[guild_id].append(source)
+                    self.queue[guild_id].append(songInfo)
                 else:
-                    self.queue[guild_id] = [source]
+                    self.queue[guild_id] = [songInfo]
+                await ctx.send("`{}` added to the queue".format(songTitle))
             else:
                 vc.play(source, after=lambda x=0: self.checkQueue(ctx, ctx.message.guild.id))
-        
-        # Send message about song
-        self.songMessage = await ctx.send(":loud_sound: `{} [{}]` - {}".format(title, duration, ctx.author.name))
+                await ctx.send(":loud_sound: `{} [{}]` - {}".format(songTitle, songDuration, ctx.author.name))
 
     @commands.command(help="")
     async def pause(self, ctx):
@@ -93,9 +105,9 @@ class MusicCommands(commands.Cog):
             return False
 
         # Check for music channel
-        # if not ctx.channel.id == musicChannelID:
-        #     await ctx.reply("Please use {} for music commands.".format(self.bot.get_channel(musicChannelID).mention))
-        #     return False
+        if not ctx.channel.id == musicChannelID:
+            await ctx.reply("Please use {} for music commands.".format(self.bot.get_channel(musicChannelID).mention))
+            return False
 
         return True
 
