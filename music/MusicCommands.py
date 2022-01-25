e549b35dbf0973bfef19b550066e57f4398c288d
MacBook <maxodell11@gmail.com>
2022-01-25 17:08:53 +1000

    add 10 minute auto-timeout

--- music/MusicCommands.py
+++ music/MusicCommands.py
@@ -2,6 +2,7 @@ import discord, youtube_dl, json
 from discord.ext import commands
 from resources import musicChannelID
 from youtube_search import YoutubeSearch
+import asyncio
 
 # Developer mode switch
 devMode = True
@@ -181,6 +182,24 @@ class MusicCommands(commands.Cog):
             queuePosition = self.queues[ctx.message.guild.id].index(song) + 1
             queueMessage += ("**{}.** {} [{}] - {}\n".format(queuePosition, song.title, song.duration, song.requestedBy))
         await ctx.send(queueMessage)
+
+    # Auto time-out after 10 minutes of not playing music
+    @commands.Cog.listener()
+    async def on_voice_state_update(self, member, before, after):
+        if not member.id == self.bot.user.id:
+            return
+        elif before.channel is None:
+            voice = after.channel.guild.voice_client
+            time = 0
+            while True:
+                await asyncio.sleep(1)
+                time = time + 1
+                if voice.is_playing() and not voice.is_paused():
+                    time = 0
+                if time == 600:
+                    await voice.disconnect()
+                if not voice.is_connected():
+                    break
 
 def setup(bot):
     bot.add_cog(MusicCommands(bot))
